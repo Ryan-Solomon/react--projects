8e950b75d233cacc4138b8ebe2c8a936484e0758
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-01-23 18:11:38 -0600

    add react router dom and import homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import HomePage from './pages/HomePage';
 
 function App() {
-  return <h1>App</h1>;
+  return (
+    <Router>
+      <Route exact path='/'>
+        <HomePage />
+      </Route>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export default function HomePage() {
+  return (
+    <HomeContainer>
+      <h1>Home Page</h1>
+    </HomeContainer>
+  );
+}
+
+const HomeContainer = styled.div``;
