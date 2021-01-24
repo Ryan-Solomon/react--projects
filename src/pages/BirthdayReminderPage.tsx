3859678b8785c65c2a084250964308de77357905
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-01-23 19:34:41 -0600

    setup data fetching on mount of BirthdayReminder

--- src/pages/BirthdayReminderPage.tsx
+++ src/pages/BirthdayReminderPage.tsx
@@ -1,7 +1,31 @@
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import { isConstructorDeclaration } from 'typescript';
 import Nav from '../components/Nav';
+import { TStatus, TUser } from '../types/types';
 
 export default function BirthdayReminderPage() {
+  const [status, setStatus] = useState<TStatus>('LOADING');
+  const [users, setUsers] = useState<TUser[]>();
+
+  useEffect(() => {
+    const getUsers = async () => {
+      try {
+        const res = await fetch('https://jsonplaceholder.typicode.com/users');
+        if (res.status !== 200) {
+          throw new Error('Status is not ok');
+        }
+        const data = await res.json();
+        setUsers(data);
+        setStatus('FULFILLED');
+      } catch (error) {
+        setStatus('ERROR');
+        console.error(error.message);
+      }
+    };
+    getUsers();
+  }, []);
+
   return (
     <Container>
       <Nav />
