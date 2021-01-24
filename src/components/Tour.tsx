ceb57a1a2c8ee10baae4f130ffdd3d50d814a123
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-01-23 20:14:38 -0600

    add tour styles

--- src/components/Tour.tsx
+++ src/components/Tour.tsx
@@ -8,5 +8,39 @@ type TProps = {
 export default function Tour({ tour }: TProps) {
   const { image, info, name, price } = tour;
 
-  return <h1>{name}</h1>;
+  return (
+    <TourContainer>
+      <TourImage src={image} alt='tour' />
+      <TourDetails>
+        <TourHeading>{name}</TourHeading>
+        <TourInfo>{info}</TourInfo>
+      </TourDetails>
+      <RemoveTourButton>Not Interested</RemoveTourButton>
+    </TourContainer>
+  );
 }
+
+const TourContainer = styled.div`
+  background: #fff;
+  width: 40rem;
+`;
+
+const TourDetails = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 2.5rem;
+`;
+
+const TourImage = styled.img`
+  width: 100%;
+`;
+
+const TourHeading = styled.h2`
+  color: #333;
+`;
+
+const TourInfo = styled.p`
+  color: #444;
+`;
+
+const RemoveTourButton = styled.button``;
