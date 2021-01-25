8b5303525f05909aca5c33e40ae97b4db5aef3d3
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-01-25 14:16:30 -0600

    woa, swr is quick

--- src/components/Movies.tsx
+++ src/components/Movies.tsx
@@ -1,8 +1,13 @@
 import styled from 'styled-components';
+import useSWR from 'swr';
 
-const url = `https://api.themoviedb.org/3/movie/latest?api_key=${process.env.REACT_APP_UPCOMING_MOVIE_SEARCH_API_KEY}<&language=en-US`;
+const url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.REACT_APP_UPCOMING_MOVIE_SEARCH_API_KEY}&language=en-US&page=1`;
+// @ts-ignore
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 export default function Movies() {
+  const { data, error } = useSWR(url, fetcher);
+  console.log(data);
   return (
     <MoviesContainer>
       <h1>Movies</h1>
