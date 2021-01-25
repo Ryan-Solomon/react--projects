d1f50e3b567f6f7d724702822491b828226d2d31
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-01-25 13:56:32 -0600

    secrets

--- src/components/Movies.tsx
+++ src/components/Movies.tsx
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 
+const url = `https://api.themoviedb.org/3/movie/latest?api_key=${process.env.REACT_APP_UPCOMING_MOVIE_SEARCH_API_KEY}<&language=en-US`;
+
 export default function Movies() {
   return (
     <MoviesContainer>
