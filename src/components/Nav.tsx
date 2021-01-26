b050b05697989bebaba2f18a461805aa84d3c601
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-01-25 19:48:08 -0600

    make nav a column;

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,9 +1,12 @@
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 export default function Nav() {
+  const [showNav, setShowNav] = useState(false);
   return (
     <NavContainer>
+      <Toggle>X</Toggle>
       <NavList>
         <Link to='/'>
           <NavItem>Home</NavItem>
@@ -29,23 +32,41 @@ export default function Nav() {
 }
 
 const NavContainer = styled.nav`
-  background: #1a1a1a;
   padding: 1.5rem;
+  display: flex;
+  color: white;
+  flex-direction: column;
   box-shadow: 2px 2px 10px #222;
+  background: #1a1a1a;
+  width: 32rem;
+  position: absolute;
+`;
+
+const Toggle = styled.div`
+  color: #d0d0d0;
+  position: absolute;
+  right: 1.5rem;
+  font-size: 2rem;
+  transition: all 0.2s ease;
+
+  &:hover {
+    transform: scale(1.1);
+    cursor: pointer;
+    color: #fff;
+  }
 `;
 
 const NavList = styled.ul`
   list-style: none;
-  display: flex;
-  color: white;
 `;
 
 const NavItem = styled.li`
   color: #d0d0d0;
   font-size: 2rem;
   letter-spacing: 0.2rem;
-  margin: 0px 2rem;
   transition: all 0.2s ease;
+  padding: 1rem;
+  width: 80%;
 
   &:hover {
     color: #fff;
