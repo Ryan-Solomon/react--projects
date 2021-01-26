7de3b3c3000b3362e7db67852fa5ff3ef20e87ff
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-01-26 11:13:47 -0600

    render out drink imgs

--- src/components/Drink.tsx
+++ src/components/Drink.tsx
@@ -7,7 +7,23 @@ type Props = {
 };
 
 export const DrinkComponent: FC<Props> = ({ drink }) => {
-  const { idDrink, strDrink, strDrinkThumb } = drink;
+  const { strDrink, strDrinkThumb } = drink;
 
-  return <h1>{strDrink}</h1>;
+  return (
+    <DrinkContainer>
+      <DrinkImage src={strDrinkThumb} alt='drink' />
+      <DrinkTitle>{strDrink}</DrinkTitle>
+    </DrinkContainer>
+  );
 };
+
+const DrinkContainer = styled.div``;
+
+const DrinkTitle = styled.h2`
+  color: #fff;
+`;
+
+const DrinkImage = styled.img`
+  width: 100%;
+  object-fit: cover;
+`;
