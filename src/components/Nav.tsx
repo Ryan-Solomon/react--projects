8b6c1a030f61688f7dedbf61a448ba87017bbee7
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-01-25 19:55:38 -0600

    hide and show nav on click

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,45 +1,57 @@
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
+import { FaBars } from 'react-icons/fa';
 
 export default function Nav() {
   const [showNav, setShowNav] = useState(false);
   return (
-    <NavContainer>
-      <Toggle>X</Toggle>
-      <NavList>
-        <Link to='/'>
-          <NavItem>Home</NavItem>
-        </Link>
-        <Link to='/birthday'>
-          <NavItem>Birthday Reminder</NavItem>
-        </Link>
-        <Link to='/tours'>
-          <NavItem>Tours</NavItem>
-        </Link>
-        <Link to='/movies'>
-          <NavItem>Movies</NavItem>
-        </Link>
-        <Link to='/accordion'>
-          <NavItem>Accordion</NavItem>
-        </Link>
-        <Link to='/groceries'>
-          <NavItem>Grocery List</NavItem>
-        </Link>
-      </NavList>
+    <NavContainer show={showNav}>
+      {showNav ? (
+        <>
+          <Toggle onClick={() => setShowNav(false)}>X</Toggle>
+          <NavList>
+            <Link to='/'>
+              <NavItem>Home</NavItem>
+            </Link>
+            <Link to='/birthday'>
+              <NavItem>Birthday Reminder</NavItem>
+            </Link>
+            <Link to='/tours'>
+              <NavItem>Tours</NavItem>
+            </Link>
+            <Link to='/movies'>
+              <NavItem>Movies</NavItem>
+            </Link>
+            <Link to='/accordion'>
+              <NavItem>Accordion</NavItem>
+            </Link>
+            <Link to='/groceries'>
+              <NavItem>Grocery List</NavItem>
+            </Link>
+          </NavList>
+        </>
+      ) : (
+        <FaBars onClick={() => setShowNav(true)} size={30} color='white' />
+      )}
     </NavContainer>
   );
 }
 
-const NavContainer = styled.nav`
+type TNavProps = {
+  show: boolean;
+};
+
+const NavContainer = styled.nav<TNavProps>`
   padding: 1.5rem;
   display: flex;
   color: white;
   flex-direction: column;
-  box-shadow: 2px 2px 10px #222;
+  box-shadow: ${({ show }) => (show ? '2px 2px 10px #222' : '')};
   background: #1a1a1a;
   width: 32rem;
   position: absolute;
+  margin-left: ${({ show }) => (show ? '' : '2rem')};
 `;
 
 const Toggle = styled.div`
