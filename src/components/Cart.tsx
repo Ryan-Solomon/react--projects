129847f60f49cd5452cd253a172e1267bc6bdc55
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-01-27 13:23:38 -0600

    render over items and display a cart item

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,5 +1,17 @@
 import styled from 'styled-components';
+import { useCartContext } from '../context/cartContext';
 
 export const Cart = () => {
-  return <h1>My Cart</h1>;
+  const { items } = useCartContext();
+
+  if (items.length < 1) return <h1>No Items homie</h1>;
+  return (
+    <CartContainer>
+      {items.map((item) => (
+        <h1 key={item.idDrink}>{item.strDrink}</h1>
+      ))}
+    </CartContainer>
+  );
 };
+
+const CartContainer = styled.div``;
